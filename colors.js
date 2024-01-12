d21a0496da8ba0be830d20399a0b962fd154511b
Jbassard <jonathanacciarito@gmail.com>
2024-01-11 22:15:26 -0500

    Version 1.1.2: Bug officially fixed, testPrint() function exports correctly

--- colors.js
+++ colors.js
@@ -105,8 +105,6 @@ const FieryText = (text) => {
   return fieryOutput + reset;
 };
 
-const { testPrint } = require("./testPrint");
-
 const colors = {
   CyanText,
   BlackText,
@@ -144,6 +142,27 @@ const colors = {
   BrightBlueBackground,
   BrightMagentaBackground,
   BrightWhiteBackground,
+};
+
+const testPrint = () => {
+  const colorFunctions = {};
+
+  // Iterate over the functions in the module
+  for (const functionName in colors) {
+    const functionValue = colors[functionName];
+
+    // Check if it's a function
+    if (typeof functionValue === "function") {
+      // Add the function to the colorFunctions object
+      colorFunctions[functionName] = functionValue;
+    }
+  }
+
+  // Now you can use colorFunctions in the loop
+  for (const [colorName, colorFunction] of Object.entries(colorFunctions)) {
+    const coloredText = colorFunction(colorName);
+    console.log(coloredText);
+  }
 };
 
 const allFunctions = {
